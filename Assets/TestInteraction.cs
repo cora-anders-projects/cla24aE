5406bd31c847405db3d69267308c5589a5e02e9a
K2anonymous <user@FIH-COM23a110.fsu.edu>
2025-04-14 14:35:23 -0400

    added terrain and final ship

---
+++ Assets/TestInteraction.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInteraction : MonoBehaviour
+{
+    public void TestGrab()
+    {
+        Debug.Log("test");
+    }
+}
